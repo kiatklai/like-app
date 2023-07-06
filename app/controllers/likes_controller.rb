c301b882b5886702ebe7620f763adb0877859b32
kiatklai <likhitthaworn.k@gmail.com>
2023-07-06 18:07:49 +0900

    learnhowtolike

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,19 @@
+class LikesController < ApplicationController
+  before_action :set_tweet
+  
+  def create
+    like = current_user.likes.build(tweet_id: params[:tweet_id])
+    like.save
+    render 'create.js.erb'
+  end
+
+  def destroy
+    like = Like.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
+    like.destroy
+    render 'destroy.js.erb'
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:tweet_id])
+  end
+end
